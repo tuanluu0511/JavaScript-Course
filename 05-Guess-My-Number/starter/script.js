8de91a534a040f7a26e041332fbaedd7071e49ca
Tuan Luu <tuanluu.hcmc@gmail.com>
2021-11-06 22:59:56 +0700

    Handle click event

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+/*
+console.log((document.querySelector('.message').textContent = 'Correct!'));
+
+document.querySelector('.number').textContent = 19;
+
+document.querySelector('.score').textContent = 16;
+
+document.querySelector('.guess').value = 12;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
