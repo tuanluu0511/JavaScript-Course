642eb4de9f74434418d886ea9fd10ab013080aea
Tuan Luu <tuanluu.hcmc@gmail.com>
2021-11-07 07:01:05 +0700

    Implement the game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,10 +10,34 @@ document.querySelector('.score').textContent = 16;
 document.querySelector('.guess').value = 12;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20 * 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
