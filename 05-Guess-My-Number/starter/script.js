27e56bbd5f5ad5c1a16588f8caaa474716e082d6
Tuan Luu <tuanluu.hcmc@gmail.com>
2021-11-07 11:38:43 +0700

    Implemente highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,11 +9,11 @@ document.querySelector('.score').textContent = 16;
 
 document.querySelector('.guess').value = 12;
 */
-
-const secretNumber = Math.trunc(Math.random() * 20 * 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
 
+// Check button:
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -23,9 +23,18 @@ document.querySelector('.check').addEventListener('click', function () {
 
     // Correct guess
   } else if (guess == secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent = 'Correct number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    score--;
+    document.querySelector('.score').textContent = score;
+
+    // High score:
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
   // Number too high
   else if (guess > secretNumber) {
@@ -49,4 +58,18 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// Again button:
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  // Use "let" will call another variable, just call the variable name out as below:
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing!';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
