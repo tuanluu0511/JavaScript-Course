16d65e8ad8951c721bb2b9ccec01cccb07f5b56c
Tuan Luu <tuanluu.hcmc@gmail.com>
2021-11-17 00:19:13 +0700

    Reset the game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -12,14 +12,35 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let score, currentScore, activePlayer, playing;
+
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
-const score = [0, 0];
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  currentScore = 0;
+  score = [0, 0];
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -38,11 +59,7 @@ btnRoll.addEventListener('click', function () {
         currentScore;
     } else {
       // Switch player
-      document.getElementById(`current--${activePlayer}`).textContent = 0;
-      currentScore = 0;
-      activePlayer = activePlayer === 0 ? 1 : 0;
-      player0El.classList.toggle('player--active');
-      player1El.classList.toggle('player--active');
+      switchPlayer();
     }
   }
 });
@@ -55,7 +72,7 @@ btnHold.addEventListener('click', function () {
       score[activePlayer];
 
     // 2. Check if the score >=100
-    if (score[activePlayer] >= 10) {
+    if (score[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -67,11 +84,35 @@ btnHold.addEventListener('click', function () {
         .classList.remove('player--active');
     } else {
       // 3. Switch to the next player
-      document.getElementById(`current--${activePlayer}`).textContent = 0;
-      currentScore = 0;
-      activePlayer = activePlayer === 0 ? 1 : 0;
-      player0El.classList.toggle('player--active');
-      player1El.classList.toggle('player--active');
+      switchPlayer();
     }
   }
 });
+
+btnNew.addEventListener('click', init);
+/*
+
+btnNew.addEventListener('click', function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.toggle('player--active');
+
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  score[0] = 0;
+  score[1] = 0;
+  currentScore = 0;
+  activePlayer = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+});
+*/
