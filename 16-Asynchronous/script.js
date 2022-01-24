702fdb8994a38de752dc8cb6c151b7bec39300e2
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-01-24 14:58:08 +0700

    Call back hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -5,17 +5,9 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `
-  <article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
           <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -30,11 +22,39 @@ const getCountryData = function (country) {
           </div>
         </article>
         `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getNeighbourCountry = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1:
+    renderCountry(data);
+
+    // Get neighbour country(2):
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+    // AJAX call country 2:
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+
+    request2.send();
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // Render neighbour country(2):
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('germany');
+getNeighbourCountry('usa');
