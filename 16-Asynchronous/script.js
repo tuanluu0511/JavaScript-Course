2f5c6652ebab9e93dbdf8474c6e1876e01401a72
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-01-24 16:30:17 +0700

    Chaining promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -64,7 +64,13 @@ getNeighbourCountry('usa');
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
-getCountryData('usa');
+getCountryData('china');
