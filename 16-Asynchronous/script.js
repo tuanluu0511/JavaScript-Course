9a4435f6944705895175111c7f31155ecfc9bcef
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-01-27 22:26:23 +0700

    Coding challenge 2

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,5 +1,124 @@
 'use strict';
 
+// CODING CHALLENGE 2:
+
+// Your tasks:
+// Tasks are not super-descriptive this time, so that you can figure out some stuff by
+// yourself. Pretend you're working on your own �
+// PART 1
+// 1. Create a function 'createImage' which receives 'imgPath' as an input.
+// This function returns a promise which creates a new image (use
+// document.createElement('img')) and sets the .src attribute to the
+// provided image path
+// 2. When the image is done loading, append it to the DOM element with the
+// 'images' class, and resolve the promise. The fulfilled value should be the
+// image element itself. In case there is an error loading the image (listen for
+// the'error' event), reject the promise
+// 3. If this part is too tricky for you, just watch the first part of the solution
+
+// PART 2
+// 4. Consume the promise using .then and also add an error handler
+// 5. After the image has loaded, pause execution for 2 seconds using the 'wait'
+// function we created earlier
+// 6. After the 2 seconds have passed, hide the current image (set display CSS
+// property to 'none'), and load a second image (Hint: Use the image element
+// returned by the 'createImage' promise to hide the current image. You will
+// need a global variable for that �)
+// 7. After the second image has loaded, pause execution for 2 seconds again
+// 8. After the 2 seconds have passed, hide the current image
+// Test data: Images in the img folder. Test the error handler by passing a wrong
+// image path. Set the network speed to “Fast 3G” in the dev tools Network tab,
+// otherwise images load too fast
+
+// Jonas code:
+const wait = second => {
+  return new Promise(resolve => {
+    setTimeout(resolve, second * 1000);
+  });
+};
+const imgContainer = document.querySelector('.images');
+
+const createImage = imgPath => {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found!'));
+    });
+  });
+};
+
+let currentImage;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImage = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImage = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    createImage('img/img-3.jpg');
+  });
+
+/*
+
+
+// Self code:
+let html, img;
+
+const imgs = document.querySelector('.images');
+const wait = second => {
+  return new Promise(resolve => {
+    setTimeout(resolve, second * 1000);
+  });
+};
+// 1 Create an createImage funciton
+const createImage = imgPath => {
+  return new Promise((resolve, reject) => {
+    img = document.createElement('img');
+    img.src = imgPath;
+
+    // Add image element to DOM
+    img.onload = () => {
+      imgs.insertAdjacentElement('beforeend', img);
+      // Resolve it
+      resolve(img);
+    };
+    img.onerror = () => reject(new Error('Image address invalid'));
+  });
+};
+
+createImage('img/img-1.jpg')
+  .then(() => {
+    return wait(2);
+  })
+  .then(() => {
+    img.style.display = 'none';
+    createImage('img/img-2.jpg');
+    return wait(2);
+  })
+  .then(() => {
+    img.style.display = 'none';
+    createImage('img/img-3.jpg');
+  })
+  .catch(err => console.log(err));
+
+
+
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
@@ -100,7 +219,6 @@ const whereAmI = function () {
 
 whereAmI();
 
-/*
 // Build a simple promise
 const lotteryPromise = new Promise(function (resolve, reject) {
   console.log(`Lottery never draw!`);
